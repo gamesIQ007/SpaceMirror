7d947021fa7ca8536dd606baf0308d03b4ccd183
gamesIQ007 <bdagames@ya.ru>
2023-07-09 19:53:31 +0300

    42.5. SyncVar

---
+++ Assets/Scripts/Vehicle/VehicleColor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Mirror;
+
+/// <summary>
+/// Цвет транспорта
+/// </summary>
+public class VehicleColor : NetworkBehaviour
+{
+    /// <summary>
+    /// Спрайт
+    /// </summary>
+    [SerializeField] private SpriteRenderer m_SpriteRenderer;
+
+    /// <summary>
+    /// Цвет
+    /// </summary>
+    [SyncVar(hook = nameof(SetColor))]
+    private Color m_Color;
+
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        m_Color = Random.ColorHSV();
+    }
+
+
+    /// <summary>
+    /// Задать цвет
+    /// </summary>
+    /// <param name="oldColor">Старый цвет</param>
+    /// <param name="newColor">Новый цвет</param>
+    private void SetColor(Color oldColor, Color newColor)
+    {
+        m_SpriteRenderer.color = newColor;
+    }
+}
